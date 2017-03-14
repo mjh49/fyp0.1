56b999cec7c9d14717bbe907fd0ebecf4636287a
Matthew Hemstock <matthewhemstock@Matthews-iMac.local>
2017-03-14 00:18:16 +0000

    Initial Commit

---
+++ FYP0.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FYP0.1
+//
+//  Created by Matthew Hemstock on 14/03/2017.
+//  Copyright © 2017 Matthew Hemstock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
